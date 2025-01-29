e14647b0c4da2b70ab11bd4a3702dba565638c4d
tomortimer <tcmortimer@outlook.com>
2025-01-29 20:21:17 +0000

    defining fiolepath

--- identifier.sh
+++ identifier.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "" > currentTracklist.txt
-for filename in ./run/user/1000/gvfs/cdda:host=sr0/; do
-    python3 aidmatch.py ./run/user/1000/gvfs/cdda:host=sr0/$filename >> currentTracklist.txt
+for filename in "/run/user/1000/gvfs/cdda:host=sr0/*"; do
+    python3 aidmatch.py "/run/user/1000/gvfs/cdda:host=sr0/$filename" >> currentTracklist.txt
 done
