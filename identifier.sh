b4d6168d7788c0f14f0f2d689200516a5d784f7c
tomortimer <tcmortimer@outlook.com>
2025-01-29 22:14:07 +0000

    restarted identifier

--- identifier.sh
+++ identifier.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "" > currentTracklist.txt
-for filename in "/run/user/1000/gvfs/cdda:host=sr0"/*; do
-    sudo python3 aidmatch.py "/run/user/1000/gvfs/cdda:host=sr0/$filename" >> currentTracklist.txt
+# Define the target directory
+directory="/run/user/1000/gvfs/cdda:host=sr0"
+
+# Check if the target is not a directory
+if [ ! -d "$directory" ]; then
+  exit 1
+fi
+
+# Loop through files in the target directory
+for file in "$directory"/*; do
+  if [ -f "$file" ]; then
+    echo "$file"
+  fi
 done
